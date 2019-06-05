5d4ac06f6c50fb7fa75016d0c5e16d3282444f24
KotDurak <DarkProkuror@mail.ru>
2019-06-05 22:57:29 +0400

    add_mikhaen_card

--- mikhaen/js/main.js
+++ mikhaen/js/main.js
@@ -4,4 +4,9 @@ $(document).ready(function() {
 	  $.mask.definitions['n'] = '[0-9]';
 	  $("#phone1").mask("+994(nnn) nnn-nnnn");
 	});
+
+	$('#show-card').on('click', function(){
+		var D_elem = $(this);
+		$('#myModal').modal('show');
+	});
 });
